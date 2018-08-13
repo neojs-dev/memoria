f4f8a19570ab9c9a2195e3a329dc07ec6be3d6f3
neosiae <neosiae@gmail.com>
2018-08-13 21:47:27 +0200

    Add save

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,6 +2,7 @@ import * as STM from './stm/stm.js';
 import * as editor from './editor.js';
 import * as notesList from './notesList.js';
 import * as scrollbar from './scrollbar.js';
+import * as database from './firebase/database.js';
 import { addClass, removeClass } from './helpers.js';
 
 const sidebar = document.querySelector('.sidebar');
@@ -19,4 +20,5 @@ export function remove(id, target) {
   editor.empty();
   scrollbar.update();
   STM.remove(id);
+  database.save();
 }
